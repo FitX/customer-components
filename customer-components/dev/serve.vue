77864a9a4934fae33a263b506b9237a0e3076fc5
Michael Gerstmann <michael.gerstmann@fitx.de>
2021-08-18 14:25:05 +0200

    [ST-1602] test rollup config

---
+++ customer-components/dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { TextButton } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  TextButton,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <text-button text="hallo" />
+  </div>
+</template>
---
+++ customer-components/src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as TextButton } from './text-button/TextButton.vue';
