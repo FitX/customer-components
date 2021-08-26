c28fd6d6a182a37d2dd9ae2769c6930c32c0b706
Michael Gerstmann <michael.gerstmann@fitx.de>
2021-08-26 14:22:33 +0200

    [ST-1610] Init Base Options

---
+++ src/components/base-option/BaseOption.stories.js
@@ -0,0 +1,108 @@
+import { reactive } from 'vue';
+import { isDarkMode } from '../../../.storybook/template-helpers/use-template-theme-detection';
+import BaseOption, {
+  modifier,
+} from './BaseOption.vue';
+
+const storyDescription = `
+  **Verwendung:**
+  Base Option Component wird verwendet um **@TODO**
+`
+
+export default {
+  title: 'Components/BaseOption',
+  component: BaseOption,
+  argTypes: {
+    onClick: {},
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: storyDescription,
+      },
+    },
+  },
+};
+
+/* ******************************** */
+/// Templates
+/* ******************************** */
+/**
+ * Base Button Template with default Slot
+ * @type {string}
+ */
+const baseTemplate = `
+<base-option v-bind="args">
+  <template v-if="args?.slotProps?.default" #default>{{ args.slotProps.default }}</template>
+</base-option>`;
+
+/**
+ * Base Buttons Group Template
+ * @type {string}
+ */
+const groupTemplate = `
+<div style="display: flex; flex-wrap: wrap; align-items: center; gap: 1rem">
+  <base-option v-for="(button, index) in group" :key="index" v-bind="button.args" />
+</div>`;
+
+/* ******************************** */
+/// Story Wrapper
+/* ******************************** */
+/**
+ * Default Template
+ * @param {object} argsObject - vue props e.g. {tag: 'button', text: 'Button', isDarkMode: false }
+ * @return {{template: string, components: {BaseButton: {setup(): {getModifierClasses: *}, props: {modifier: {default: null, validator: function(*=): (boolean|boolean), type: [StringConstructor, ArrayConstructor]}, isDarkMode: {default: boolean, type: Boolean | BooleanConstructor}, tag: {default: string, type: String | StringConstructor}, text: {default: null, type: String | StringConstructor}}}}, setup(): {args: *}}}
+ * @constructor
+ */
+const Template = (argsObject) => ({
+  setup() {
+    const args = argsObject;
+    if (typeof args.isDarkMode === 'undefined') {
+      args.isDarkMode = isDarkMode;
+    }
+    return {
+      args: reactive(args),
+    };
+  },
+  components: { BaseOption },
+  template: baseTemplate,
+});
+
+/**
+ * Group Template
+ * @param {array} groupItems - vue props e.g. [{ args: {tag: 'button', text: 'Button', isDarkMode: false } }]
+ * @return {{template: string, components: {BaseButton: {setup(): {getModifierClasses: *}, props: {modifier: {default: null, validator: function(*=): (boolean|boolean), type: [StringConstructor, ArrayConstructor]}, isDarkMode: {default: boolean, type: Boolean | BooleanConstructor}, tag: {default: string, type: String | StringConstructor}, text: {default: null, type: String | StringConstructor}}}}, setup(): {args: *}}}
+ * @constructor
+ */
+const TemplateGroup = (groupItems) => ({
+  components: { BaseOption },
+  setup() {
+    const group = groupItems.map((item) => {
+      const itemCopy = item;
+      if (typeof item.args.isDarkMode === 'undefined') {
+        itemCopy.args.isDarkMode = isDarkMode;
+      }
+      return itemCopy;
+    });
+    return {
+      group: reactive(group),
+    };
+  },
+  template: groupTemplate,
+});
+
+//* ******************************** */
+/// Stories
+/* ******************************** */
+export const DefaultBaseOption = Template.bind({});
+DefaultBaseOption.args = {
+  text: 'Option',
+};
+
+export const DefaultBaseOption = () => TemplateGroup([
+  {
+    args: {
+      text: 'Button 1',
+    },
+  },
+]);
---
+++ src/components/base-option/BaseOption.vue
@@ -0,0 +1,222 @@
+<template>
+  <component
+    :is="tag"
+    v-bind="$attrs"
+    :class="[ getModifierClasses('btn', modifier), { 'btn--dark' : isDarkMode } ]"
+    class="btn">
+    <slot>
+      {{ text }}
+    </slot>
+    <span class="btn__loading" />
+  </component>
+</template>
+
+<script>
+import useModifier from '@/use/modifier-class';
+
+export const modifier = [
+  'secondary',
+  'tertiary',
+  'quaternary',
+  'disabled',
+  'small',
+  'fake-hover',
+];
+
+export default {
+  props: {
+    tag: {
+      type: String,
+      default: 'button',
+    },
+    text: {
+      type: String,
+      default: null,
+    },
+    isDarkMode: {
+      type: Boolean,
+      default: false,
+    },
+    modifier: {
+      type: [String, Array],
+      default: null,
+      validator: (value) => {
+        if (typeof value === 'object') {
+          const filtered = modifier.filter((mod) => value.includes(mod));
+          return filtered.length > 0;
+        }
+        return modifier.includes(value);
+      },
+    },
+  },
+  setup() {
+    const { getModifierClasses } = useModifier();
+    return {
+      getModifierClasses,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+// @use currently only with dart-sass
+// @use '~@/assets/styles/mixins.scss' as mixin;
+@import '~@/assets/styles/mixin-reset.scss';
+.btn {
+  @include btn-reset();
+
+  $self: &;
+  --btn-color-bg: var(--primary-brand-color-orange, #ED6A12);
+  --btn-color-border: var(--btn-color-bg);
+  --btn-color: #fff;
+  --btn-font-size: 1.8rem;
+  --btn-padding: 1.5rem;
+  --btn-border-width: 2px;
+  --loader-size: 1.5rem;
+  position: relative;
+  width: 28rem;
+  max-width: 100%;
+  font-size: var(--btn-font-size);
+  font-weight: 400;
+  background: var(--btn-color-bg);
+  color: var(--btn-color);
+  padding: var(--btn-padding);
+  border: var(--btn-border-width) solid var(--btn-color-border);
+  border-radius: 2.6rem;
+  cursor: pointer;
+  text-align: center;
+  &:hover,
+  &--fake-hover {
+    --btn-color-bg: var(--brand-color-orange-1);
+  }
+
+  &:disabled,
+  &--disabled {
+    &,
+    &:hover {
+      --btn-color-bg: var(--brand-color-gray-chalk);
+      --btn-color: var(--brand-color-gray-cement);
+      cursor: not-allowed;
+    }
+    &#{$self}--dark {
+      &,
+      &:hover {
+        --btn-color-bg: var(--brand-color-anthracite-3);
+        --btn-color: var(--brand-color-gray-cement);
+      }
+    }
+  }
+
+  &--small {
+    --btn-font-size: 1.6rem;
+    --btn-padding: 0.7rem;
+    --btn-border-width: 1px;
+    padding: var(--btn-padding) calc(3 * var(--btn-padding));
+    display: inline-block;
+    width: auto;
+  }
+
+  &--secondary {
+    --btn-color-bg: transparent;
+    --btn-color-border: var(--brand-color-anthracite);
+    --btn-color: var(--brand-color-anthracite);
+
+    &:hover,
+    &#{$self}--fake-hover {
+      --btn-color-bg: var(--brand-color-gray-chalk);
+    }
+    &:disabled,
+    &#{$self}--disabled {
+      &,
+      &:hover {
+        --btn-color-bg: transparent;
+        --btn-color: var(--brand-color-gray-cement);
+        --btn-color-border: var(--brand-color-gray-ash);
+      }
+    }
+
+    &#{$self}--dark {
+      --btn-color-bg: transparent;
+      --btn-color: #fff;
+      --btn-color-border: #fff;
+
+      &:hover,
+      &#{$self}--fake-hover {
+        --btn-color-bg: var(--brand-color-anthracite-3);
+      }
+
+      &:disabled,
+      &#{$self}--disabled {
+        &,
+        &:hover {
+          --btn-color-bg: transparent;
+          --btn-color: var(--brand-color-gray-cement);
+          --btn-color-border: var(--brand-color-anthracite-3);
+        }
+      }
+    }
+  }
+
+  &--tertiary {
+    --btn-color-bg: transparent;
+    --btn-color-border: transparent;
+    --btn-color: var(--brand-color-anthracite);
+
+    &:hover,
+    &#{$self}--fake-hover {
+      --btn-color-bg: var(--brand-color-gray-chalk);
+    }
+    &:disabled,
+    &#{$self}--disabled {
+      --btn-color: var(--brand-color-gray-cement);
+    }
+    &#{$self}--dark {
+      --btn-color: #fff;
+      &:hover,
+      &#{$self}--fake-hover {
+        --btn-color-bg: var(--brand-color-anthracite-3);
+      }
+    }
+  }
+
+  &--quaternary {
+    --btn-color-bg: #fff;
+    --btn-color-border: #fff;
+    --btn-color: var(--brand-color-anthracite);
+
+    &:hover,
+    &#{$self}--fake-hover {
+      --btn-color-border: var(--brand-color-gray-chalk);
+      --btn-color-bg: var(--brand-color-gray-chalk);
+    }
+  }
+
+  &:active #{&}__loading {
+    display: inline-block;
+    width: var(--loader-size);
+    height: var(--loader-size);
+    position: absolute;
+    right: 1.8rem;
+    top: 50%;
+    transform: translate3d(0, -50%, 0);
+    &:after {
+      content: '';
+      display: block;
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      border: 0.2rem solid #fff;
+      border-color: #fff transparent #fff transparent;
+      animation: ring 1s linear infinite;
+    }
+    @keyframes ring {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  }
+}
+</style>
