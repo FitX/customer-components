3f16b7f578d01a1c63e048988414f304955a7062
Frank Holder <frank.holder@FXMPC02CQ39YMD6R.local>
2021-08-30 18:19:14 +0200

    add code coverage

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,11 @@ module.exports = {
   transform: {
     '^.+\\.vue$': 'vue-jest',
   },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    './src/components/**/*.{js,vue}',
+    '!./src/components/**/*.stories.js',
+    '!./src/components/**/*.{e2e,spec}.js',
+    './src/use/**/*.js',
+  ],
 };
