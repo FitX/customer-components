cd91a5b35e177867fd61f8966c709fbc549b2a20
Frank Holder <frank.holder@FXMPC02CQ39YMD6R.local>
2021-08-30 18:20:34 +0200

    [ST-1606] add tests

---
+++ tests/unit/components/BaseButton.spec.js
@@ -0,0 +1,32 @@
+import { shallowMount } from '@vue/test-utils';
+import '@testing-library/jest-dom';
+import BaseButton from '@/components/base-button/BaseButton.vue';
+
+describe('Base Button', () => {
+  it('renders as button as default', () => {
+    const wrapper = shallowMount(BaseButton);
+    expect(wrapper.html()).toMatch(`<button class="btn"><span class="btn__loading"></span></button>`);
+  });
+
+  it('renders as link', () => {
+    const wrapper = shallowMount(BaseButton, {
+      props: { tag: 'a' },
+    });
+    expect(wrapper.html()).toMatch(`<a class="btn"><span class="btn__loading"></span></a>`);
+  });
+
+  it('shows text', () => {
+    const wrapper = shallowMount(BaseButton, {
+      props: { text: 'hello world' },
+    });
+    expect(wrapper.text()).toContain('hello world');
+  });
+
+  it('has correct modifier classes', () => {
+    const wrapper = shallowMount(BaseButton, {
+      props: { modifier: 'secondary' },
+    });
+    const button = wrapper.find('button');
+    expect(button.element).toHaveClass('btn--secondary');
+  });
+});
