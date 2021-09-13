b5dd96ef7b6f7210bb5b228e18bea33939d18393
Michael Gerstmann <michael.gerstmann@fitx.de>
2021-09-13 14:56:42 +0200

    [ST-1607] change build config to vite

--- src/index.js
+++ src/index.js
@@ -34,11 +34,27 @@ import BaseInput from '@/components/base-input/BaseInput.vue';
 import BaseButton from '@/components/base-button/BaseButton.vue';
 import BaseOption from '@/components/base-option/BaseOption.vue';
 import BaseOptionForm from '@/components/base-option-form/BaseOptionForm.vue';
+import '@/assets/styles/lib.scss';
 
-export {
+const components = {
   TextButton,
   BaseButton,
   BaseOption,
   BaseOptionForm,
   BaseInput,
 };
+
+function install(instance) {
+  // eslint-disable-next-line guard-for-in,no-restricted-syntax
+  for (const component in components) {
+    instance.component(components[component].name, components[component]);
+  }
+}
+
+export default { install };
+
+export { default as TextButton } from '@/components/text-button/TextButton.vue';
+export { default as BaseInput } from '@/components/base-input/BaseInput.vue';
+export { default as BaseButton } from '@/components/base-button/BaseButton.vue';
+export { default as BaseOption } from '@/components/base-option/BaseOption.vue';
+export { default as BaseOptionForm } from '@/components/base-option-form/BaseOptionForm.vue';
