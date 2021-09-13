c943796c850270a7b72b94afab55f65f2f8ad83b
Michael Gerstmann <michael.gerstmann@fitx.de>
2021-09-13 09:47:08 +0200

    [ST-1607] update readme install instructions, update index

--- README.md
+++ README.md
@@ -87,7 +87,8 @@ Go to consuming App, link and install
 npm link @fitx/customer-components
 npm i @fitx/customer-components
 ```
-
+## Update Lib
+Make sure to update [index file](src/index.js) after add new Components.
 ## Publish Lib
 ```
 npm publish --access public
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import TextButton from '@/components/text-button/TextButton.vue';
+import BaseButton from '@/components/base-button/BaseButton.vue';
+import BaseOption from '@/components/base-option/BaseOption.vue';
+import BaseOptionForm from '@/components/base-option-form/BaseOptionForm.vue';
 
 /* eslint-disable import/prefer-default-export */
 export {
   TextButton,
+  BaseButton,
+  BaseOption,
+  BaseOptionForm,
 };
