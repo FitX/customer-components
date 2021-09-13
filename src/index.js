20dd6c960a32d7d5bcabf723988b61cfb51e62ca
Michael Gerstmann <michael.gerstmann@fitx.de>
2021-09-13 14:23:14 +0200

    [ST-1607] change build config to vite

--- src/index.js
+++ src/index.js
@@ -27,4 +27,18 @@ export {
   BaseOptionForm,
   BaseInput,
 }; */
-export * from './components/index';
+// export * from './components/index';
+
+import TextButton from '@/components/text-button/TextButton.vue';
+import BaseInput from '@/components/base-input/BaseInput.vue';
+import BaseButton from '@/components/base-button/BaseButton.vue';
+import BaseOption from '@/components/base-option/BaseOption.vue';
+import BaseOptionForm from '@/components/base-option-form/BaseOptionForm.vue';
+
+export {
+  TextButton,
+  BaseButton,
+  BaseOption,
+  BaseOptionForm,
+  BaseInput,
+};
