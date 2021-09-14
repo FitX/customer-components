4de3b1cbccd78fcd7224f9b098c37677b38cbf1d
Michael Gerstmann <michael.gerstmann@fitx.de>
2021-09-14 12:21:22 +0200

    [ST-1607] reduce export for testing

--- src/components/index.js
+++ src/components/index.js
@@ -16,8 +16,8 @@ export {
 
 export { default as ErrorMessage } from '@/components/error-message/ErrorMessage.vue';
 export { default as ErrorIcon } from '@/components/error-icon/ErrorIcon.vue';
-export { default as TextButton } from '@/components/text-button/TextButton.vue';
+/* export { default as TextButton } from '@/components/text-button/TextButton.vue';
 export { default as BaseInput } from '@/components/base-input/BaseInput.vue';
 export { default as BaseButton } from '@/components/base-button/BaseButton.vue';
 export { default as BaseOption } from '@/components/base-option/BaseOption.vue';
-export { default as BaseOptionForm } from '@/components/base-option-form/BaseOptionForm.vue';
+export { default as BaseOptionForm } from '@/components/base-option-form/BaseOptionForm.vue'; */
