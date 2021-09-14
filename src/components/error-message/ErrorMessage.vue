ed3943065d04d203d9f3fa43e6051ccf2a450c9b
Michael Gerstmann <michael.gerstmann@fitx.de>
2021-09-14 12:19:24 +0200

    [ST-1607] test scss without alias

--- src/components/error-message/ErrorMessage.vue
+++ src/components/error-message/ErrorMessage.vue
@@ -29,7 +29,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import '~@/assets/styles/colors.scss';
+@import '../../assets/styles/colors.scss';
 .error-message {
   @include functional-colors();
   color: var(--functional-color-error);
