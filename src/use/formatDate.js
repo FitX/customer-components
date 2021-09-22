c65a77a9986d152f02ed05ec16729ac799b0c87f
Frank Holder <frank.holder@FXMPC02CQ39YMD6R.fritz.box>
2021-09-22 14:43:55 +0200

    add format function in its own file

---
+++ src/use/formatDate.js
@@ -0,0 +1,31 @@
+/**
+ * Format dotted Date to Iso
+ * @param {String} dottedDate - Format 'dd.MM.yyyy'
+ * @return {string|null}
+ */
+export function formatDateToIso(dottedDate = '') {
+  if (dottedDate?.toString().length === 10) {
+    const d = dottedDate.substring(0, 2);
+    const m = dottedDate.substring(3, 5);
+    const y = dottedDate.substring(6, 10);
+    return `${y}-${m}-${d}`;
+  }
+  return null;
+}
+
+/**
+ * Format Iso Date to dotted
+ * @param {String} isoDate - Format 'yyyy-MM-dd'
+ * @return {string|null}
+ */
+export function formatDateFromIso(isoDate = '') {
+  console.log('iso', isoDate);
+  if (isoDate?.toString().length === 10) {
+    const y = isoDate.substring(0, 4);
+    const m = isoDate.substring(5, 7);
+    const d = isoDate.substring(8, 10);
+    console.log('iso', `${d}.${m}.${y}`);
+    return `${d}.${m}.${y}`;
+  }
+  return null;
+}
