0ee768dac508363e5c4f1bf89ac8dc3c3433705a
Michael Gerstmann <michael.gerstmann@fitx.de>
2021-09-27 13:41:53 +0200

    [ST-1780] demo

---
+++ src/components/icons.stories.jsx
@@ -0,0 +1,50 @@
+import { ref } from 'vue';
+import IconCheckmark from '@/assets/icons/icon-checkmark.svg';
+
+const iconList = [
+  {
+    component: IconCheckmark,
+    name: 'IconCheckmark',
+    source: 'https://icons8.com/icon/3061/checkmark',
+  },
+];
+
+export default {
+  title: 'Styleguide/Icons',
+  // component: ColorPalette,
+  parameters: {
+    options: {
+      showPanel: false,
+    },
+  },
+};
+
+export const Colors = () => ({
+  setup() {
+    const detail = ref({});
+    function showDetail(componentName) {
+      detail.value = iconList
+        .filter((icon) => icon.name === componentName)?.[0];
+    }
+    return {
+      detail,
+      showDetail,
+      iconList,
+    };
+  },
+  template: `
+  <section>
+    <h1>{{ detail.name }}</h1>
+    <a :href="detail.source" target="_blank" v-if="detail.source">Quelle</a>
+  </section>
+  <ul style="list-style: none; margin: 0; padding: 0; display: flex; flex-wrap: wrap;">
+    <li
+      v-for="icon in iconList"
+      :index="icon.name"
+      style="border: 1px solid red; line-height: 0;"
+      @click="showDetail(icon.name)">
+      <component :is="icon.component" />
+    </li>
+  </ul>
+  `,
+});
