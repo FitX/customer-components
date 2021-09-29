b8b9d95c47b0403073039b0128f2a1289892eb83
Michael Gerstmann <michael.gerstmann@fitx.de>
2021-09-29 12:48:35 +0200

    [ST-1780] icon documentation

--- src/components/icons.stories.jsx
+++ src/components/icons.stories.jsx
@@ -1,56 +1,105 @@
-import { ref } from 'vue';
 import IconCheckmark from '@/assets/icons/icon-checkmark.svg';
-import IconAttention from '@/assets/icons/icon-attention.svg';
+import IconError from '@/assets/icons/icon-error.svg';
+import IconClear from '@/assets/icons/icon-clear.svg';
+import IconClose from '@/assets/icons/icon-close.svg';
+import IconBack from '@/assets/icons/icon-back.svg';
+import IconInfo from '@/assets/icons/icon-info.svg';
+import IconList from '@/doc-helper/IconList.vue';
+
+const CATEGORY_ENUMS = {
+  SYSTEM: 'System Icons',
+};
+
+const storyDescription = `
+  **Verwendung:**
+
+  - Angelegt zentriert im Rahmen 18 x 18 px
+  - Icongröße darin optisch angepasst
+  - Namen auf Englisch
+  - Default Farbwert anthrazit
+`;
 
 const iconList = [
   {
     component: IconCheckmark,
     name: 'Checkmark',
-    source: 'https://icons8.com/icon/3061/checkmark',
+    source: {
+      name: 'icons8',
+      url: 'https://icons8.com/icon/3061/checkmark',
+    },
+    category: CATEGORY_ENUMS.SYSTEM,
   },
   {
-    component: IconAttention,
-    name: 'Attention',
-    source: 'https://icons8.com/icon/11691/box-important',
-  }
+    component: IconError,
+    name: 'Error',
+    source: {
+      name: 'icons8',
+      url: 'https://icons8.com/icon/11691/box-important',
+    },
+    category: CATEGORY_ENUMS.SYSTEM,
+  },
+  {
+    component: IconClear,
+    name: 'Clear',
+    source: {
+      name: 'icons8',
+      url: 'https://icons8.com/icon/59754/cancel',
+    },
+    category: CATEGORY_ENUMS.SYSTEM,
+  },
+  {
+    component: IconClose,
+    name: 'Close',
+    source: {
+      name: 'icons8',
+      url: 'https://icons8.com/icon/83149/delete',
+    },
+    category: CATEGORY_ENUMS.SYSTEM,
+  },
+  {
+    component: IconBack,
+    name: 'Back',
+    source: {
+      name: 'icons8',
+      url: 'https://icons8.com/icon/set/back/ios-filled',
+    },
+    category: CATEGORY_ENUMS.SYSTEM,
+  },
+  {
+    component: IconInfo,
+    name: 'Info',
+    source: {
+      name: 'icons8',
+      url: 'https://icons8.com/icon/77/info',
+    },
+    category: CATEGORY_ENUMS.SYSTEM,
+  },
 ];
-
 export default {
   title: 'Styleguide/Icons',
-  // component: ColorPalette,
+  // component: IconList,
   parameters: {
     options: {
       showPanel: false,
     },
+    docs: {
+      description: {
+        component: storyDescription,
+      },
+    },
   },
 };
 
-export const Colors = () => ({
+export const Icons = () => ({
+  components: {
+    IconList,
+    // ColorSlider,
+  },
   setup() {
-    const detail = ref({});
-    function showDetail(componentName) {
-      detail.value = iconList
-        .filter((icon) => icon.name === componentName)?.[0];
-    }
     return {
-      detail,
-      showDetail,
       iconList,
     };
   },
   template: `
-  <section>
-    <h1>{{ detail.name }}</h1>
-    <a :href="detail.source" target="_blank" v-if="detail.source">Quelle</a>
-  </section>
-  <ul style="list-style: none; margin: 0; padding: 0; display: flex; flex-wrap: wrap;">
-    <li
-      v-for="icon in iconList"
-      :index="icon.name"
-      style="border: 1px solid red; line-height: 0;"
-      @click="showDetail(icon.name)">
-      <component :is="icon.component" />
-    </li>
-  </ul>
-  `,
+    <icon-list :icons="iconList"/>`,
 });
