b92ee71df61b27d554aef22da202af7fd4679db3
Michael Gerstmann <michael.gerstmann@fitx.de>
2021-10-15 12:59:40 +0200

    [ST-1782] automatic export

--- src/index-wc.js
+++ src/index-wc.js
@@ -2,6 +2,7 @@ import '@/assets/styles/lib.scss';
 import { defineCustomElement } from 'vue';
 import * as components from './components/index';
 
+
 /**
  * Convert camelCase to kebab-case
  * @param {string} string
@@ -14,63 +15,22 @@ const transformKebabCase = (string) => string
     ? `${idx !== 0 ? '-' : ''}${letter.toLowerCase()}`
     : letter)).join('');
 
-// components collection
-export const componentsCollection = { ...components };
+const objectMap = (obj, fn) => Object.fromEntries(
+  Object.entries(obj).map(
+    ([k, v], i) => [k, fn(v, k, i)],
+  ),
+);
 
-const componentsDesc = Object.keys(componentsCollection).map((item) => {
-  const component = componentsCollection[item];
-  const name = component.name || 'c-comp';
-  return {
-    [name]: defineCustomElement(component),
-  };
-});
+export const webComponents = objectMap(components, (v) => defineCustomElement(v));
 
-/**
- * Install Component as Web Component
- * @param {string} tagName - web component name in kebab-case
- * @param component - Vue Component
- */
-const installWebComponent = function (tagName, component) {
-  if (!tagName || !component) {
-    return;
-  }
-  const validName = transformKebabCase(tagName);
-  window.customElements.define(validName, component);
-};
-
-/**
- * Register all Components as Web Component, prefix with 'web-'
- */
-export const installWebComponents = function () {
-  componentsDesc.forEach((item) => {
-    installWebComponent(`web-${item.name}`, item.component);
-  });
-};
-
-// components desc array
-export const webComponents = [...Object.entries(componentsCollection)].map(([key, value]) => ({
-  [key]: defineCustomElement(value),
-}));
-
-/**
- * Global install Function
- * @param app - Vue instance
- */
 export function register() {
-  webComponents.forEach((item) => {
-    const kebabCaseName = transformKebabCase(item.name);
-    const registerComponent = item.component;
-    window.customElements.define(kebabCaseName, registerComponent); // kebab-case
-  });
+  Object
+    .fromEntries(
+      Object
+        .entries(webComponents)
+        .map(([k, v]) => [k, customElements.define(transformKebabCase(k), v)]),
+    );
 }
-
-/*
-export default {
-  ...componentsDesc.map((comp) => ({
-    [comp.name]: comp.component,
-  })),
-};
-*/
 
 export default {
   ...webComponents,
