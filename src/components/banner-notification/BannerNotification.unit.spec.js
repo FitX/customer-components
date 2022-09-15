0b11a345fc45010545fdbf97879066377901673d
Michael Gerstmann <michael.gerstmann@fitx.de>
2022-09-15 11:45:51 +0200

    [ST-2156] implement timeout handling for duration, adjust tests

--- src/components/banner-notification/BannerNotification.unit.spec.js
+++ src/components/banner-notification/BannerNotification.unit.spec.js
@@ -1,8 +1,9 @@
-import { shallowMount } from '@vue/test-utils';
+import { flushPromises, shallowMount } from '@vue/test-utils';
 import '@testing-library/jest-dom';
 import BannerNotification from '@/components/banner-notification/BannerNotification.vue';
 
 describe('Notification Banner', () => {
+  jest.useFakeTimers();
   it('renders optional html content', async () => {
     const wrapper = shallowMount(BannerNotification, {
       slots: {
@@ -10,5 +11,19 @@ describe('Notification Banner', () => {
       },
     });
     expect(wrapper.html()).toContain('<p>foo bar</p>');
+  });
+  it('dont emit on missing duration', async () => {
+    const wrapper = shallowMount(BannerNotification, {});
+    jest.runAllTimers();
+    expect(wrapper.emitted('duration-ends')).toBeFalsy();
+  });
+  it('emits if duration is greater then 0', async () => {
+    const wrapper = shallowMount(BannerNotification, {
+      props: {
+        duration: 100,
+      },
+    });
+    jest.runAllTimers();
+    expect(wrapper.emitted('duration-ends')).toBeTruthy();
   });
 });
