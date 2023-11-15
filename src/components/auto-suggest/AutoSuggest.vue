06ae372bda52222bcacd37a8319ab2db8ad4bbc0
Michael Raguse <michael.raguse@fitx.de>
2023-11-15 14:29:02 +0100

    [GYMX-477] sr only helper

--- src/components/auto-suggest/AutoSuggest.vue
+++ src/components/auto-suggest/AutoSuggest.vue
@@ -276,16 +276,7 @@ onMounted(() => {
   max-height: 26.1875rem; // 419
   overflow-y: auto;
   &:not(&--is-expanded) {
-    // visibility: hidden;
-    position: absolute;
-    width: 1px;
-    height: 1px;
-    padding: 0;
-    margin: -1px;
-    overflow: hidden;
-    clip: rect(0, 0, 0, 0);
-    white-space: nowrap;
-    border-width: 0;
+    @include reset.sr-only();
   }
 
   &__item {
