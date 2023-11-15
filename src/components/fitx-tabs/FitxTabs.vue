6560e6fe1e7621bc52476e1b794aa732259d5775
Michael Raguse <michael.raguse@fitx.de>
2023-11-15 12:04:14 +0100

    [GYMX-476] prepare tabs

--- src/components/fitx-tabs/FitxTabs.vue
+++ src/components/fitx-tabs/FitxTabs.vue
@@ -1,5 +1,12 @@
 <script setup>
-import { ref, onMounted, defineProps, computed, useAttrs } from 'vue';
+import { useActiveElement } from '@vueuse/core';
+import {
+  ref,
+  defineProps,
+  computed,
+  useAttrs,
+  unref, onMounted,
+} from 'vue';
 
 /*
 type Tab = { title: string, content: string, }
@@ -10,110 +17,100 @@ const props = defineProps<{
 const props = defineProps({
   tabs: {
     type: Array,
-    default: () => [{ title: '1', content: 'content 1' }],
+    default: () => [{ title: '1', content: 'content 1' }, { title: '2', content: 'content 2' }],
     required: true,
   },
 });
 
 const attrs = useAttrs();
+const activeElement = useActiveElement();
+
+const DEFAULT_ID_NAME = 'fitx-tab';
+const tabsContainerEl = ref();
+const componentId = computed(() => attrs?.id || DEFAULT_ID_NAME);
+
 const generateId = (index) => {
   const { id } = attrs;
   if (id) return `${id}-${index}`;
-  return `fitx-tabs-${index};`
+  return `${unref(componentId)}-${index};`;
 };
 
 const tabs = computed(() => props.tabs.map((tab, index) => ({ id: generateId(index), ...tab })));
+const selectedTabIndex = ref(0);
 
-/**
- *
- * @type {ToRef<HTMLElement[]>}
- */
-const tabsRef = ref();
-/**
- * @type {ToRef<HTMLElement[]>}
- */
-const tabpanelsRef = ref([]);
-
-/* interface TabListItem {
-  tab: HTMLElement;
-  tabpanel: HTMLElement;
-} */
-
-/**
- *
- * @type {ToRef<TabListItem[]>}
- */
-const tabs = ref([]);
-
-/**
- *
- * @param {HTMLElement} tab
- */
-const setSelectedTab = (tab) => {
-  for (const otherTabs of tabs.value) {
-    otherTabs.tab.setAttribute('aria-selected', 'false');
-    otherTabs.tab.tabIndex = -1;
-    otherTabs.tabpanel.classList.add('is-hidden');
+const getNextElement = (nodes) => {
+  // eslint-disable-next-line no-underscore-dangle
+  const _activeElement = unref(activeElement);
+  if (nodes.length > Number(_activeElement.dataset.tabTriggerIndex)) {
+    return nodes
+      .find((node) => (
+        Number(node.dataset.tabTriggerIndex) === (Number(_activeElement.dataset.tabTriggerIndex) + 1)));
   }
+  return nodes[0];
+};
 
-  tab.setAttribute('aria-selected', 'true');
-  tab.removeAttribute('tabindex');
-  tab.tabpanel.classList.remove('is-hidden');
+const getPrevElement = (nodes) => {
+  // eslint-disable-next-line no-underscore-dangle
+  const _activeElement = unref(activeElement);
+  if (Number(_activeElement.dataset.tabTriggerIndex) > 0) {
+    return nodes
+      .find((node) => (
+        Number(node.dataset.tabTriggerIndex) === (Number(_activeElement.dataset.tabTriggerIndex) - 1)));
+  }
+  return nodes[0];
+};
 
-  tab.focus();
+const selectTabIndex = (index) => {
+  const {
+    isNext,
+    // isPrev,
+  } = {
+    isNext: unref(selectedTabIndex) < index,
+    isPrev: unref(selectedTabIndex) > index,
+  };
+  const newIndex = unref(tabs).length < index ? 0 : index;
+  selectedTabIndex.value = newIndex;
+  const resultElements = [...unref(tabsContainerEl).childNodes].filter((node) => node?.role === 'tab');
+  console.log('resultElements', resultElements);
+  const el = isNext ? getNextElement(resultElements) : getPrevElement(resultElements);
+  el.focus();
 };
 
 onMounted(() => {
-  const tablist = tabsRef.value;
-  tabs.value = tablist.querySelectorAll('[role=tab]').map((tab) => {
-    const tabpanel = document.getElementById(tab.getAttribute('aria-controls'));
-    return { tab, tabpanel };
-  });
-
-  tablist.addEventListener('keydown', (event) => {
-    // const tgt = event.currentTarget as HTMLElement;
-    /**
-     * @type {C | EventTarget}
-     */
-    const tgt = event.currentTarget;
-    switch (event.key) {
-      case 'ArrowLeft':
-        setSelectedTab(tabs.value[tabs.value.indexOf(tgt) - 1].tab);
-        break;
-      case 'ArrowRight':
-        setSelectedTab(tabs.value[tabs.value.indexOf(tgt) + 1].tab);
-        break;
-      case 'Home':
-        setSelectedTab(tabs.value[0].tab);
-        break;
-      case 'End':
-        setSelectedTab(tabs.value[tabs.value.length - 1].tab);
-        break;
-      default:
-        break;
-    }
-  });
+  console.log(tabsContainerEl.value);
 });
 </script>
 
 <template>
-  <div role="tablist" class="automatic">
-    <template
-        v-for="{ tab, tabpanel } in tabs" :key="tab">
-      <button
-          :id="tab.id"
-          :aria-controls="tabpanel.id"
-          role="tab"
-          @click="setSelectedTab(tab)"
-      >
-        {{ tab.textContent }}
-      </button>
+  <div class="tabs">
+      <div
+          ref="tabsContainerEl"
+          role="tablist"
+          :aria-labelledby="`${componentId}`">
+        <button
+            v-for="(tab, index) in tabs"
+            :key="tab.id"
+            :id="tab.id"
+            :data-tab-trigger-index="index"
+            type="button"
+            role="tab"
+            @keydown.right.prevent.stop="selectTabIndex((index + 1))"
+            @keydown.left.prevent.stop="selectTabIndex((index - 1))"
+            :aria-selected="selectedTabIndex === index"
+            :tabindex="selectedTabIndex === index ? undefined : -1"
+            :aria-controls="`${componentId}-content-${index}`">
+          {{ tab.title }}
+        </button>
+      </div>
 
       <div
-          :key="tabpanel.id"
-          :id="tabpanel.id" class="tabpanel is-hidden">
-        {{ tabpanel.textContent }}
+          v-for="(tab, index) in tabs"
+          :key="tab.id"
+          :id="`${componentId}-content-${index}`"
+          role="tabpanel"
+          tabindex="0"
+          :aria-labelledby="tab.id">
+        {{ tab.content }}
       </div>
-    </template>
   </div>
 </template>
