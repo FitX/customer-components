3eda8a7320ee315eafc20af438ec578a8df49707
Michael Raguse <michael.raguse@vipion.de>
2024-04-15 11:14:22 +0200

    [GYMX-591] cleanup for project setup

--- tests/unit/setup.js
+++
@@ -1,7 +0,0 @@
-global.console = {
-  log: console.log,
-  error: console.error,
-  warn: jest.fn(), // overwrite native behaviour to reduce noise
-  info: console.info,
-  debug: console.debug,
-};
