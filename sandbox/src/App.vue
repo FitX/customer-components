24e7ac309d014bd45aaa9a3a588d857272d8441e
Michael Raguse <michael.raguse@vipion.de>
2024-04-16 12:19:30 +0200

    [GYMX-591] vue ts config

---
+++ sandbox/src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import {
+  FitxButton
+} from '@fitx/customer-components';
+</script>
+
+<template>
+  <main>
+    <h1>Components</h1>
+    <fitx-button msg="fooooo" />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
