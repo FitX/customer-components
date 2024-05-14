225ccd8c39dc33ade6c0426969c07aef0689aca7
Michael Raguse <michael.raguse@vipion.de>
2024-05-14 13:04:27 +0200

    [GYMX-601] with defaults for better sb support, add is active prop

--- src/components/fitx-button/fitx-button.vue
+++ src/components/fitx-button/fitx-button.vue
@@ -21,19 +21,17 @@ import { computed } from 'vue';
 
 // type FakeStates = 'hover' | 'focus' | 'active';
 // export type ButtonState = 'disabled' | 'primary' | 'secondary' | 'tertiary' | 'quaternary';
-const {
-  theme,
-  tag = 'button',
-  size = 'default',
-  modifier,
-  fakeModifier,
-} = defineProps<{
+const props = withDefaults(defineProps<{
   theme?: Theme,
   tag?: 'button' | 'span',
   size?: ButtonSize,
+  isActive?: boolean,
   modifier?: ButtonState | ButtonState[],
   fakeModifier?: 'hover' | 'focus' | 'active', // Dev Mode only @TODO remove from export on build
-}>();
+}>(), {
+  tag: 'button',
+  size: 'default',
+});
 
 /* const componentClasses = computed(() => {
   const sizes = size ? [`size-${size}`] : [];
@@ -50,21 +48,31 @@ const {
  */
 const componentClasses = computed(() => (
   [
-    ...getModifierClasses('button', size ? `size-${size}` : []),
-    ...getModifierClasses('button', modifier ?? []),
-    ...getModifierClasses('button', fakeModifier ? `fake-${fakeModifier}` : []),
+    ...getModifierClasses('button', props.size ? `size-${props.size}` : []),
+    ...getModifierClasses('button', props.modifier ?? []),
+    ...getModifierClasses('button', props.fakeModifier ? `fake-${props.fakeModifier}` : []),
+    ...getModifierClasses('button', props.isActive ? 'is-active' : []),
   ]
 ));
 </script>
 <template>
   <component
-    :is="tag"
-    :data-theme="theme"
+    :is="props.tag"
+    :data-theme="props.theme"
     class="button"
     :class="componentClasses">
-    <slot name="icon-start"></slot>
-    <slot name="default">{{ modifier }}</slot>
-    <slot name="icon-end" />
+    <!--
+    @slot optional Icon Slot
+    -->
+    <slot name="icon-start">
+      <span />
+    </slot>
+    <slot name="default">{{ props.modifier }}</slot>
+    <slot name="icon-end">
+      <span
+        v-show="props.isActive"
+        class="button__loading" />
+    </slot>
   </component>
 </template>
 
@@ -80,6 +88,8 @@ const componentClasses = computed(() => (
   --_button-radius: var(--button-radius, var(--radius-10));
   --_button-border-width: var(--button-border-width, 2px);
   --_button-font-size: var(--button-font-size, 1.125rem);
+  --_button-loader-size: var(--button-loader-size, calc(var(--_button-font-size) / 2));
+  --_button-content-gap: var(--button-content-gap, var(--size-px-2));
   // primary Styles
   --_button-color-surface: var(--button-color-primary-surface-light, var(--brand-color-orange-0));
   --_button-color-border: var(--button-color-primary-border-light, var(--brand-color-orange-0));
@@ -168,7 +178,12 @@ const componentClasses = computed(() => (
     --_button-color-text-disabled: var(--button-color-quaternary-text-disabled-light, var(--brand-color-gray-cement));
   }
 
-  display: inline-block;
+  // display: inline-block;
+  display: inline-grid;
+  grid-template-columns: var(--_button-loader-size) 1fr var(--_button-loader-size);
+  gap: var(--_button-content-gap);
+  align-items: center;
+
   background: var(--_button-color-surface);
   color: var(--_button-color-text);
   font-size: var(--_button-font-size);
@@ -186,6 +201,37 @@ const componentClasses = computed(() => (
     --_button-color-surface: var(--_button-color-surface-disabled);
     --_button-color-text: var(--_button-color-text-disabled);
     --_button-color-border: var(--_button-color-border-disabled);
+  }
+
+  &--is-active {
+    transition: 0.4s ease grid-template-columns;
+
+    #{$self}__loading {
+      display: inline-block;
+      aspect-ratio: 1;
+      max-height: var(--_button-loader-size);
+      right: var(--_button-loader-size);
+
+      &:after {
+        content: '';
+        display: block;
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        border: 0.2rem solid #fff;
+        border-color: #fff transparent #fff transparent;
+        animation: ring 1s linear infinite;
+      }
+
+      @keyframes ring {
+        0% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+    }
   }
 }
 </style>
