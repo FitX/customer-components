9d082bb0161f2c6c41de4b6b8533d742290bbfd1
Michael Raguse <michael.raguse@vipion.de>
2024-05-24 12:05:00 +0200

    [GYMX-849] fix config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,8 +12,10 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/vue3-vite',
     options: {
-      docgen: 'vue-component-meta',
-      tsconfig: 'tsconfig.app.json',
+      docgen: {
+        plugin: 'vue-component-meta',
+        tsconfig: 'tsconfig.app.json',
+      },
     }
   },
   docs: {}
