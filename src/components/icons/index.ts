0afca958da22355e7954ca79ddcd6008aaaf283c
Michael Raguse <michael.raguse@vipion.de>
2024-06-05 11:52:09 +0200

    [GYMX-809] add checkmark icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,11 @@
 import IconBack from '@/assets/icons/back.svg';
 import IconClear from '@/assets/icons/clear.svg';
 import IconClose from '@/assets/icons/close.svg';
+import IconCheckmark from '@/assets/icons/checkmark.svg';
 
-export { IconClear, IconBack, IconClose };
+export {
+  IconClear,
+  IconBack,
+  IconClose,
+  IconCheckmark,
+};
