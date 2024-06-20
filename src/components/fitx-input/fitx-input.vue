0c327366e9f12c3da520bdf51835e609ca8c47a1
Michael Raguse <michael.raguse@vipion.de>
2024-06-20 12:00:45 +0200

    [GYMX-604] griddy again

--- src/components/fitx-input/fitx-input.vue
+++ src/components/fitx-input/fitx-input.vue
@@ -40,37 +40,42 @@ const componentClasses = computed(() => [
 <style lang="scss" scoped>
 .input {
   $self: &;
-  --_input-block-size: var(--input-block-size, 3.75rem); // 3.75rem
+  --_input-block-size: var(--input-block-size, 3.75rem); // 60px
   --_input-spacing-inline: var(--input-spacing-inline, 1.125rem);
   --_input-spacing-block: var(--input-spacing-block, 0.625rem);
   --_input-radius: var(--input-radius, var(--radius-4));
+
   --_input-color-border: var(--input-color-border-light, var(--brand-color-gray-stone));
   --_input-color-input: var(--input-color-input-light, var(--brand-color-anthracite-0));
   --_input-color-label: var(--input-color-label-light, var(--brand-color-gray-carbon));
-  --_input-font-size-input: var(--input-font-size-input, 1.125rem);
 
-  --_input-label-size: 100%;
+  --_input-font-size-input: var(--input-font-size-input, 1.125rem);
+  --_input-font-size-label: var(--_input-font-size-input);
+  --_input-transition-duration: var(--input-transition-duration, 200ms);
+
+  --_input-label-size: 100%; // overlapping 2 rows
 
   &__ui {
     display: grid;
     grid-template-rows: var(--_input-label-size) calc(100% - var(--_input-label-size));
     grid-template-columns: 100%;
     block-size: var(--_input-block-size);
-    background: coral;
     padding-block: var(--_input-spacing-block);
     padding-inline: var(--_input-spacing-inline);
-    transition: grid-template-rows 200ms ease;
+    transition: grid-template-rows var(--_input-transition-duration) ease;
     align-content: center;
     overflow: hidden;
     border: 1px solid var(--_input-color-border);
     border-radius: var(--_input-radius);
+    font-weight: 400;
 
     /**
     @TODO replace because browser support has
      */
     &:has(input:focus),
     &:has(input:not(:placeholder-shown)){
-      --_input-label-size: 1.25rem;
+      --_input-label-size: 1rem;
+      --_input-font-size-label: 0.875rem;
     }
   }
 
@@ -83,9 +88,11 @@ const componentClasses = computed(() => [
     height: 100%;
     display: grid;
     align-items: center;
+    transition: font-size var(--_input-transition-duration) ease;
   }
 
   &__input {
+    font: inherit;
     font-size: var(--_input-font-size-input);
     grid-column: 1;
     padding: 0;
