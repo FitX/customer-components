b05630f2fea2eeb95e4f61763fc07877138d0bf2
Michael Raguse <michael.raguse@vipion.de>
2024-08-13 11:58:39 +0200

    [GYMX-1015] icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,7 +1,17 @@
 import IconBack from '@/assets/icons/back.svg';
-import IconClear from '@/assets/icons/clear.svg';
-import IconClose from '@/assets/icons/close.svg';
-import IconCheckmark from '@/assets/icons/checkmark.svg';
-import IconExclamationmark from '@/assets/icons/exclamationmark.svg';
+import IconKey from '@/assets/icons/key.svg';
+import IconQuestionMark from '@/assets/icons/question-mark.svg';
+import IconFilter from '@/assets/icons/filter.svg';
+import IconForward from'@/assets/icons/forward.svg';
+import IconArrowLeft from '@/assets/icons/arrow-left.svg';
+import IconArrowRight from '@/assets/icons/arrow-right.svg';
 
-export { IconClear, IconBack, IconClose, IconCheckmark, IconExclamationmark };
+export {
+  IconBack,
+  IconKey,
+  IconQuestionMark,
+  IconFilter,
+  IconForward,
+  IconArrowLeft,
+  IconArrowRight
+};
