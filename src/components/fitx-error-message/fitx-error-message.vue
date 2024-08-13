fe5293ba299cc22740d9c020c0f32df2f1dcbd6f
Michael Raguse <michael.raguse@vipion.de>
2024-08-13 17:27:42 +0200

    [GYMX-1015] test implementation gymx-ui

--- src/components/fitx-error-message/fitx-error-message.vue
+++ src/components/fitx-error-message/fitx-error-message.vue
@@ -1,3 +1,7 @@
-<script lang="ts" setup></script>
-<template>Error Message</template>
+<script lang="ts" setup>
+import { GymxButton } from '@fitx/gymx-ui';
+import { IconError } from '@/components/icons';
+
+</script>
+<template>Error Message <icon-error /><gymx-button>Test</gymx-button></template>
 <style scoped></style>
