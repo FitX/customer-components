31214e4cbd8c4e9029e90da2d70dbd427dda6c90
Michael Raguse <michael.raguse@vipion.de>
2024-08-13 17:23:25 +0200

    [GYMX-1015] create icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,17 +1,82 @@
-import IconBack from '@/assets/icons/back.svg';
-import IconKey from '@/assets/icons/key.svg';
-import IconQuestionMark from '@/assets/icons/question-mark.svg';
-import IconFilter from '@/assets/icons/filter.svg';
-import IconForward from'@/assets/icons/forward.svg';
-import IconArrowLeft from '@/assets/icons/arrow-left.svg';
-import IconArrowRight from '@/assets/icons/arrow-right.svg';
-
-export {
-  IconBack,
-  IconKey,
-  IconQuestionMark,
-  IconFilter,
-  IconForward,
-  IconArrowLeft,
-  IconArrowRight
-};
+export { default as Icon360View } from '@/assets/icons/360-view.svg';
+export { default as IconAdd } from '@/assets/icons/add.svg';
+export { default as IconApple } from '@/assets/icons/apple.svg';
+export { default as IconArrowLeft } from '@/assets/icons/arrow-left.svg';
+export { default as IconArrowRight } from '@/assets/icons/arrow-right.svg';
+export { default as IconBack } from '@/assets/icons/back.svg';
+export { default as IconBookmark } from '@/assets/icons/bookmark.svg';
+export { default as IconBrightnessRegular } from '@/assets/icons/brightness-regular.svg';
+export { default as IconCalendar } from '@/assets/icons/calendar.svg';
+export { default as IconChange } from '@/assets/icons/change.svg';
+export { default as IconChat } from '@/assets/icons/chat.svg';
+export { default as IconCheckbox } from '@/assets/icons/checkbox.svg';
+export { default as IconCheckedIdentificationDocuments } from '@/assets/icons/checked-identification-documents.svg';
+export { default as IconCheckmark } from '@/assets/icons/checkmark.svg';
+export { default as IconCircledPlay } from '@/assets/icons/circled-play.svg';
+export { default as IconClearRegular } from '@/assets/icons/clear-regular.svg';
+export { default as IconClear } from '@/assets/icons/clear.svg';
+export { default as IconClock } from '@/assets/icons/clock.svg';
+export { default as IconClose } from '@/assets/icons/close.svg';
+export { default as IconCompass } from '@/assets/icons/compass.svg';
+export { default as IconContentCopy } from '@/assets/icons/content-copy.svg';
+export { default as IconCreditCard } from '@/assets/icons/credit-card.svg';
+export { default as IconDelete } from '@/assets/icons/delete.svg';
+export { default as IconDocument } from '@/assets/icons/document.svg';
+export { default as IconDonutSmall } from '@/assets/icons/donut-small.svg';
+export { default as IconDots } from '@/assets/icons/dots.svg';
+export { default as IconDownload } from '@/assets/icons/download.svg';
+export { default as IconDumbbell } from '@/assets/icons/dumbbell.svg';
+export { default as IconEdit } from '@/assets/icons/edit.svg';
+export { default as IconEntranceRegular } from '@/assets/icons/entrance-regular.svg';
+export { default as IconError } from '@/assets/icons/error.svg';
+export { default as IconExitRegular } from '@/assets/icons/exit-regular.svg';
+export { default as IconExpand } from '@/assets/icons/expand.svg';
+export { default as IconEye } from '@/assets/icons/eye.svg';
+export { default as IconFeedbackOutline } from '@/assets/icons/feedback-outline.svg';
+export { default as IconFile } from '@/assets/icons/file.svg';
+export { default as IconFilter } from '@/assets/icons/filter.svg';
+export { default as IconForward } from '@/assets/icons/forward.svg';
+export { default as IconHeartRegular } from '@/assets/icons/heart-regular.svg';
+export { default as IconHide } from '@/assets/icons/hide.svg';
+export { default as IconHistory } from '@/assets/icons/history.svg';
+export { default as IconHome } from '@/assets/icons/home.svg';
+export { default as IconInfo } from '@/assets/icons/info.svg';
+export { default as IconKey } from '@/assets/icons/key.svg';
+export { default as IconKurs } from '@/assets/icons/kurs.svg';
+export { default as IconLink } from '@/assets/icons/link.svg';
+export { default as IconLocked } from '@/assets/icons/locked.svg';
+export { default as IconMail } from '@/assets/icons/mail.svg';
+export { default as IconMaleUser } from '@/assets/icons/male-user.svg';
+export { default as IconMap } from '@/assets/icons/map.svg';
+export { default as IconMarkerX } from '@/assets/icons/marker-x.svg';
+export { default as IconMarker } from '@/assets/icons/marker.svg';
+export { default as IconMenu } from '@/assets/icons/menu.svg';
+export { default as IconMinus } from '@/assets/icons/minus.svg';
+export { default as IconMuscle } from '@/assets/icons/muscle.svg';
+export { default as IconMute } from '@/assets/icons/mute.svg';
+export { default as IconNotification } from '@/assets/icons/notification.svg';
+export { default as IconOpenDoor } from '@/assets/icons/open-door.svg';
+export { default as IconPhone } from '@/assets/icons/phone.svg';
+export { default as IconPlus } from '@/assets/icons/plus.svg';
+export { default as IconPrintRegular } from '@/assets/icons/print-regular.svg';
+export { default as IconQuestionMark } from '@/assets/icons/question-mark.svg';
+export { default as IconQuestion } from '@/assets/icons/question.svg';
+export { default as IconRefresh } from '@/assets/icons/refresh.svg';
+export { default as IconRegularDocument } from '@/assets/icons/regular-document.svg';
+export { default as IconReset } from '@/assets/icons/reset.svg';
+export { default as IconSafety } from '@/assets/icons/safety.svg';
+export { default as IconScaleRegular } from '@/assets/icons/scale-regular.svg';
+export { default as IconSearch } from '@/assets/icons/search.svg';
+export { default as IconSettings } from '@/assets/icons/settings.svg';
+export { default as IconSignal } from '@/assets/icons/signal.svg';
+export { default as IconSound } from '@/assets/icons/sound.svg';
+export { default as IconStar } from '@/assets/icons/star.svg';
+export { default as IconSupport } from '@/assets/icons/support.svg';
+export { default as IconTicketPlay } from '@/assets/icons/ticket-play.svg';
+export { default as IconUnlocked } from '@/assets/icons/unlocked.svg';
+export { default as IconUpload } from '@/assets/icons/upload.svg';
+export { default as IconUser } from '@/assets/icons/user.svg';
+export { default as IconVideoPlay } from '@/assets/icons/video-play.svg';
+export { default as IconWifiOff } from '@/assets/icons/wifi-off.svg';
+export { default as IconWifiUnknown } from '@/assets/icons/wifi-unknown.svg';
+export { default as IconWifi } from '@/assets/icons/wifi.svg';
