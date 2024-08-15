e2114cb443c87a90ec82722b027e1a8fd75d9b73
Michael Raguse <michael.raguse@vipion.de>
2024-08-15 16:05:32 +0200

    [GYMX-1019] theme and sb bug fix

--- .storybook/docs-theme-provider.tsx
+++ .storybook/docs-theme-provider.tsx
@@ -1,4 +1,5 @@
-import { type DocsContextProps, DocsContainer } from '@storybook/blocks';
+import { type DocsContextProps, DocsContainer, Story } from '@storybook/blocks';
+import { themes } from '@storybook/theming';
 import { type PropsWithChildren } from 'react';
 import * as React from 'react';
 
@@ -12,9 +13,14 @@ export function ThemedContainer(props: PropsWithChildren<{ context: DocsContextP
   const theme = (props.context as any).store.globals.globals.theme;
   document.documentElement.setAttribute('data-theme', theme);
 
+  themes.dark.textColor = '#fff';
+
+  const isDark = (themeName) => ['dark', 'touchpoint-dark'].includes(themeName);
+
   return (
     <DocsContainer
       context={props.context}
+      theme={isDark(theme) ? themes.dark : themes.light}
       data-theme={theme}
     ><div className={'custom-docs-content'}>{props.children}</div></DocsContainer>
   )
