608c092702370c7487d3ef85137835f7d1375ba4
Michael Raguse <michael.raguse@vipion.de>
2024-09-03 14:58:59 +0200

    [GYMX-1016] optimize create component script

--- utils/create-component.ts
+++ utils/create-component.ts
@@ -47,7 +47,63 @@ describe('${componentName}', () => {
 <script lang="ts" setup></script>
 <template>${libNamePrefix}-${name}</template>
 <style lang="scss" scoped></style>`,
+  story: (name) => {
+    const componentName = `${toPascalCase(libNamePrefix)}${toPascalCase(name)}`;
+    return `
+import type { Meta, StoryObj } from '@storybook/vue3';
+import { default as ${componentName} } from './${libNamePrefix}-${name}.vue';
+import ContentDe from './readme.de.md?raw';
+// import ContentEn from './readme.en.md?raw';
+import { getMarkdownDocsTranslationBlock } from '../../../utils/docs-translation';
+
+const ComponentDescription = getMarkdownDocsTranslationBlock(ContentDe);
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta = {
+  title: 'components/WIP/${name}',
+  component: ${componentName},
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        //story: ComponentDescription,
+        component: ComponentDescription,
+      },
+    },
+  },
+} satisfies Meta<typeof ${componentName}>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Default: Story = {
+  args: {
+    // text: 'This is an error message',
+  },
 };
+`
+  },
+  readme: () => `
+#### Beschreibung:
+
+@TODO
+
+#### Verwendung:
+
+@TODO
+
+#### Verhalten:
+
+@TODO
+
+  `,
+};
+
 
 function createComponent(name: string): void {
   // Verzeichnis- und Dateinamen definieren
@@ -57,7 +113,9 @@ function createComponent(name: string): void {
     { name: 'index.ts', content: contentTemplates.index(name) },
     { name: 'types.ts', content: contentTemplates.types(name) },
     { name: `${libNamePrefix}-${name}.test.ts`, content: contentTemplates.test(name) },
-    { name: `${libNamePrefix}-${name}.vue`, content: contentTemplates.vue(name) }
+    { name: `${libNamePrefix}-${name}.vue`, content: contentTemplates.vue(name) },
+    { name: `${libNamePrefix}-${name}.stories.ts`, content: contentTemplates.story(name) },
+    { name: `readme.de.md`, content: contentTemplates.readme() },
   ];
 
   if (fs.existsSync(dirPath)) {
